8dcf5176318978c98cf82356a7ca327dd3191eaa
Ady Ngom <ady.touba.ngom@gmail.com>
2020-02-06 00:33:06 -0500

    first draft custom hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useEffect, useState } from 'react';
 import './App.css';
+import { useGroupBy } from './hooks/groupBy';
 
-function App() {
+
+ const request = async (url, options = null) => {
+   const response = await fetch(url);
+   const json = await response.json();
+   return json;
+ }
+
+  const countriesUrl = `https://api.myjson.com/bins/yz4o2`;
+
+const App = () => {
+  const [countries, setCountries] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const groups = useGroupBy(countries, 'subregion', 'asc');
+  useEffect(() => {
+    request(countriesUrl)
+    .then( payload => {
+      setCountries(payload);
+      setLoading(false);
+    })
+    .catch( err => {
+      throw new Error(`could not load countries data: ${err.toString()}`)
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {
+        isLoading ? <p>loading countries...</p>
+        : <div className="countries">
+          {
+            groups.order.map( region => {
+              return <section className="countries-region" key={region}>
+                  <header>
+                    <h1>{region}</h1>
+                  </header>
+                  {groups[region].map( country => {
+                    return <article key={country}>
+                      <h4>{country.name}</h4>  
+                    </article>
+                  })}
+                </section>
+            })
+          }
+        </div>
+      }
+    </Fragment>  
   );
 }
 
---
+++ src/hooks/groupBy.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const sortDesc = (a,b) => {
+	return (a > b) ? -1 : (b > a) ? 1 : 0;
+};
+
+export const useGroupBy = (arrObj, key, order = "desc") => {
+
+    if(!arrObj) return;
+    
+    const toc = arrObj.reduce((a,c) => {
+        const safeKey = c[key].toLowerCase();
+        !!(a[safeKey]) ? a[safeKey] = [...a[safeKey], c] : a[safeKey] = [c];
+        return a;
+    }, {});
+
+    const tocKeys = Object.keys(toc);
+    toc['order'] = (order === "desc") ? tocKeys.sort(sortDesc) : tocKeys.sort();
+    
+    return toc;
+}
